381cb63e82bbd5a9670d7f8180b5116a98a64da2
Brian Faust <brian@cline.sh>
2025-12-02 13:42:46 +0200

    first commit
    Signed-off-by: Brian Faust <brian@cline.sh>

---
+++ src/Exceptions/TogglException.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+/**
+ * Copyright (C) Brian Faust
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cline\Toggl\Exceptions;
+
+use Throwable;
+
+/**
+ * Marker interface for all Toggl package exceptions.
+ *
+ * Consumers can catch this interface to handle any exception
+ * thrown by the Toggl package.
+ */
+interface TogglException extends Throwable
+{
+    // Marker interface - no methods required
+}
